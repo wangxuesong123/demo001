f568789c37650aa8a313a7327db8443db2a4a07b
wangxuesong123 <hygj_2006@163.com>
2018-03-16 12:30:44 +0800

    调整全局变量

--- LdapRest/src/main/java/com/zznode/restapi/UserController.java
+++ LdapRest/src/main/java/com/zznode/restapi/UserController.java
@@ -20,19 +20,20 @@ import io.swagger.annotations.ApiParam;
 @RequestMapping("/zznode")
 public class UserController {
 
+	private static final String SUCESS="0";
+	private static final String ERROR="1";
 	@ApiOperation(value = "LDAP同步新增用户")
 	@PostMapping("/user")
 	public String create(@ApiParam("用户账号") @RequestParam(value = "uid") String uid,@RequestParam(value = "displayName") String displayName,@RequestParam(value = "mail") String mail) {
 
-		String message = "0";
 		// 1.根据传过来的uid参数进行业务逻辑判断
 
 		if (StringUtils.isEmpty(uid)) {
-			return message;
+			return ERROR;
 		} else {
 			// 1 调用业务逻辑层进行业务的处理
 		}
-		return message;
+		return SUCESS;
 	}
 
 	@ApiOperation(value = "LDAP同步删除用户")

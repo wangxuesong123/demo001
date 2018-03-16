bbd2725b591e8ab46f1804c1c9a62665268a49f6
wangxuesong123 <hygj_2006@163.com>
2018-03-16 12:26:38 +0800

    接受的参数发生变化

--- LdapRest/src/main/java/com/zznode/restapi/UserController.java
+++ LdapRest/src/main/java/com/zznode/restapi/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
 
 	@ApiOperation(value = "LDAP同步新增用户")
 	@PostMapping("/user")
-	public String create(@ApiParam("用户账号") @RequestParam(value = "uid") String uid) {
+	public String create(@ApiParam("用户账号") @RequestParam(value = "uid") String uid,@RequestParam(value = "displayName") String displayName,@RequestParam(value = "mail") String mail) {
 
 		String message = "0";
 		// 1.根据传过来的uid参数进行业务逻辑判断
